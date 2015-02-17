56f7d9892b60f7ae8c282dcf5d75192a11a7a007
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-17 13:07:41 +0100

    Update getsamplesummary.py

--- scripts/getsamplesummary.py
+++ scripts/getsamplesummary.py
@@ -10,16 +10,16 @@ import os
 import select
 from access import db
 
+fc_samples = {}
 def getsamplesfromflowcell(flwc):
-  print flwc
   samples = glob.glob(pars['DEMUXDIR'] + "*" + flwc + "*/Unaligned/Project_*/Sample_*")
   for sampl in samples:
-    smpl = sampl.split("/")[len(sampl.split("/"))-1].split("_")[1]
-    print smpl
-  return samples
+    sample = sampl.split("/")[len(sampl.split("/"))-1].split("_")[1]
+    fc_samples[sample] = ''
+  return fc_samples
 
 fc = "flowcell"
-print sys.argv[1]
+
 if len(sys.argv) > 0:
   try:
     sys.argv[1]
@@ -34,7 +34,5 @@ pars = db.readconfig("non")
 
 smpls = getsamplesfromflowcell(fc)
 
-
-print "Hi"
-
-
+for sample in smpls.iterkeys():
+  print sample
