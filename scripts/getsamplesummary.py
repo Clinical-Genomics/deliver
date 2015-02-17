026aed5f867261aa98510aef9154f50d27052df4
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-17 12:55:53 +0100

    Update getsamplesummary.py

--- scripts/getsamplesummary.py
+++ scripts/getsamplesummary.py
@@ -14,7 +14,7 @@ def getsamplesfromflowcell(flwc):
   print flwc
   samples = glob.glob(pars['DEMUXDIR'] + "*" + flwc + "*/Unaligned/Project_*/Sample_*")
   for sampl in samples:
-    smpl = sampl.split("/")[len(sampl.split("/"))-1]
+    smpl = sampl.split("/")[len(sampl.split("/"))-1].split("_")[1]
     print smpl
   return samples
 
