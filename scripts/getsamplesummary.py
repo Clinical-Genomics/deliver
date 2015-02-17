393d4fe771ded8e374cca34be9085d0778cdae73
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-17 11:22:40 +0100

    Create getsamplesummary.py

---
+++ scripts/getsamplesummary.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+import sys
+import datetime
+import time
+import glob
+import re
+import socket
+import os
+import select
+from db import *
+from lims import *
+
+print "Hi"
+
+
