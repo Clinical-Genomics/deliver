8cdc0b352d8b7fb9a351c8d5d5c3a2beea59adae
Rikard <rikard.erlandsson@scilifelab.se>
2015-03-30 11:17:41 +0200

    Update createfastqln.py

--- scripts/createfastqln.py
+++ scripts/createfastqln.py
@@ -30,3 +30,19 @@ with db.create_tunnel(pars['TUNNELCMD']):
     else:
       print "Correct db " + pars['STATSDB'] + " v:" + pars['DBVERSION']
 
+    flowc = sys.argv[2]
+    query = """ SELECT sample.samplename AS smp, flowcell.flowcellname AS flc, 
+    GROUP_CONCAT(unaligned.lane ORDER BY unaligned.lane) AS lanes, 
+    GROUP_CONCAT(unaligned.readcounts ORDER BY unaligned.lane) AS rds, SUM(unaligned.readcounts) AS readsum, 
+    GROUP_CONCAT(unaligned.yield_mb ORDER BY unaligned.lane) AS yield, SUM(unaligned.yield_mb) AS yieldsum, 
+    GROUP_CONCAT(TRUNCATE(q30_bases_pct,2) ORDER BY unaligned.lane) AS q30, 
+    GROUP_CONCAT(TRUNCATE(mean_quality_score,2) ORDER BY unaligned.lane) AS meanq
+    FROM sample, flowcell, unaligned, project, demux WHERE sample.sample_id = unaligned.sample_id 
+    AND flowcell.flowcell_id = demux.flowcell_id AND unaligned.demux_id = demux.demux_id 
+    AND sample.project_id = project.project_id AND project.projectname = '""" + proje + """' 
+    AND flowcell.flowcellname = '""" + flowc + """' GROUP BY samplename, flowcell.flowcell_id 
+    ORDER BY lane, sample.samplename, flowcellname """
+    hits = dbc.generalquery(query)
+    print "sample\tFlowcell\tLanes\treadcounts/lane\tsum_readcounts\tyieldMB/lane\tsum_yield\t%Q30\tMeanQscore"
+    for hit in hits:
+      print hit['smp'] + "\t" + hit['flc'] + "\t" + hit['lanes'] + "\t" + hit['rds'] + "\t" + str(hit['readsum']) + "\t" + str(hit['yield']) + "\t" + str(hit['yieldsum']) + "\t" + str(hit['q30']) + "\t" + str(hit['meanq'])
