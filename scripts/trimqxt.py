48be67bd800dc7b372bcf8963f0218bb9e89c489
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-04-25 22:56:09 +0200

    Add auto trimming.
    Works, but still need to rename the files
    and link them into the right directory

---
+++ scripts/trimqxt.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import glob
+import os
+import subprocess
+import tempfile
+from access import db, lims
+
+def get_sample_names(rundir):
+  """TODO: Docstring for getsamplesfromrun.
+
+  Args:
+      rundir (TODO): TODO
+
+  Returns: TODO
+  """
+  return get_sample_paths(rundir).keys()
+
+def get_sample_paths(rundir):
+  """TODO: Docstring for get_sample_paths.
+
+  Args:
+      rundir (TODO): TODO
+
+  Returns (dict): sample: path
+
+  """
+  sample_paths = glob.glob("{rundir}/Unalign*/Project_*/Sample_*".\
+    format(rundir=rundir))
+  samples = {}
+  for sample_path in sample_paths:
+    sample = sample_path.split("/")[-1].split("_")[1]
+    sample = sample.rstrip('BF') # remove the reprep (B) and reception control fail (F) letters from the samplename
+    samples[sample] = sample_path
+  return samples
+
+def launch_trim(indir, outdir):
+    """TODO: Docstring for launch_trim
+
+    Args:
+        arg1 (TODO): TODO
+
+    Returns: TODO
+
+    """
+    script_dir = os.path.join(indir, 'scripts')
+    try:
+      os.makedirs(script_dir)
+    except OSError: pass
+    for f1 in glob.glob('{}/*_R1_*.fastq.gz'.format(indir)):
+      f2 = f1.replace('_R1_', '_R2_')
+      outfile = '{}/{}'.format(outdir, f1.split('/')[-1].replace('.fastq.gz', ''))
+      with tempfile.NamedTemporaryFile(delete=False, dir=script_dir) as sbatch_file:
+        sbatch_file.write('#!/bin/bash')
+        sbatch_file.write('java -jar /mnt/hds/proj/bioinfo/SCRIPTS/AgilentReadTrimmer.jar -m1 {f1} -m2 {f2} -o {outfile} -qxt && gzip {outfile}*'.format(f1=f1, f2=f2, outfile=outfile))
+        sbatch_file.flush()
+        try:
+          cmd = 'sbatch -A prod001 -t 12:00:00 -J fastqTrimming -c 1 -o {sbatch_output} -e {sbatch_error}'.\
+            format(
+              sbatch_output='/mnt/hds/proj/bioinfo/LOG/fastqTrimming.%j.out',
+              sbatch_error='/mnt/hds/proj/bioinfo/LOG/fastqTrimming.%j.err'
+            )
+          cl = cmd.split(' ')
+          cl.append(sbatch_file.name)
+          print(' '.join(cl))
+          subprocess.Popen(cl)
+        except subprocess.CalledProcessError, e:
+          e.message = "The command {} failed.".format(' '.join(cl))
+          raise e
+    pass
+
+def main(argv):
+  """Takes one param: the run dir"""
+  rundir = argv[0]
+
+  # get all samples of this run
+  samples = get_sample_paths(rundir)
+  print('Found {} samples: {}'.format(len(samples), samples))
+  # determine which samples are QXT
+  params = db.readconfig("non")
+  for sample, sample_path in samples.items():
+    with lims.limsconnect(params['apiuser'], params['apipass'], params['baseuri']) as lmc:
+      application_tag = lmc.getattribute('samples', sample, "Sequencing Analysis")
+      if application_tag is None:
+        print("FATAL: Sequencing application tag not defined for {}".format(sample))
+        # skip to the next sample
+        continue
+      kit_type = application_tag[3:6]
+      if kit_type == 'QXT':
+        print('Sample {} is QXT! Trimming ...'.format(sample))
+
+        # TODO check in clinstatsdb if this sample is trimmed already ...
+
+        # move the samples to a totrim dir so they don't get picked up by next steps ...
+        sample_path_split = sample_path.split('/')
+        trim_dir = '/'.join(sample_path_split[0:-1]) + '/totrim'
+        outdir   = '/'.join(sample_path_split[0:-1]) + '/trimmed'
+        fastq_dir = sample_path_split[-1]
+        fastq_trim_dir = os.path.join(trim_dir, fastq_dir)
+        fastq_outdir   = os.path.join(outdir, fastq_dir)
+
+        # skip this sample if the input dir exists
+        if os.path.exists(fastq_trim_dir):
+          print('{} exists, skipping!'.format(fastq_trim_dir))
+          continue
+        # skip the sample if output dir not empty
+        if os.path.exists(fastq_outdir):
+          for dir_path, dir_names, files in os.walk(fastq_outdir):
+            if files:
+              print('{} not empty, skipping!'.format(fastq_outdir))
+              continue
+
+        # create input dir
+        try:
+          # only create trim_dir, the fastq_dir will me moved in here
+          print('mkdir {}'.format(trim_dir))
+          os.makedirs(trim_dir)
+        except OSError: pass
+
+        # create the output dir
+        try:
+          print('mkdir {}'.format(fastq_outdir))
+          os.makedirs(fastq_outdir)
+        except OSError: pass
+
+        # move the fastq files to the totrim dir
+        print('mv {} {}'.format(sample_path, trim_dir))
+        os.rename(sample_path, fastq_trim_dir)
+
+        # lauch trim!
+        launch_trim(fastq_trim_dir, fastq_outdir)
+
+  # mv original samples away
+  # mv trimmed samples back, append _trimmed to name
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
