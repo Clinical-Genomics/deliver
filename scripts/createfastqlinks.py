ca0429e086eeed08c8abfbe37aefdba49570890e
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-07-31 23:30:55 +0200

    Bump version: 0.6.3 → 0.6.4

--- scripts/createfastqlinks.py
+++ scripts/createfastqlinks.py
@@ -11,7 +11,7 @@ from access import db
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 def getsamplesfromflowcell(demuxdir, flwc):
   samples = glob.glob("{demuxdir}*{flowcell}/Unalign*/Project_*/Sample_*".\
--- scripts/trimqxt.py
+++ scripts/trimqxt.py
@@ -9,7 +9,7 @@ import tempfile
 import datetime
 from access import db, lims
 
-__version__ = '0.6.3'
+__version__ = '0.6.4'
 
 logger = logging.getLogger(__name__)
 
