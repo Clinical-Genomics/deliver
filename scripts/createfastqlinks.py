e018c102d8a3e18c80cbe67c5ec89322546ad143
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-09-29 13:29:09 +0200

    Bump version: 0.8.0 → 0.8.1

--- scripts/createfastqlinks.py
+++ scripts/createfastqlinks.py
@@ -11,7 +11,7 @@ from access import db
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 def getsamplesfromflowcell(demuxdir, flwc):
   samples = glob.glob("{demuxdir}*{flowcell}/Unalign*/Project_*/Sample_*".\
