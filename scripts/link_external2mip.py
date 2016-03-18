1828873bc7c519b3ffdf8cb352efb12606e6d315
kenny billiau <kenny.billiau@scilifelab.se>
2016-03-18 15:25:13 +0100

    Bump version: 1.11.0 → 1.12.0

--- scripts/link_external2mip.py
+++ scripts/link_external2mip.py
@@ -12,7 +12,7 @@ from glob import glob
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '1.11.0'
+__version__ = '1.12.0'
 
 logger = logging.getLogger(__name__)
 
