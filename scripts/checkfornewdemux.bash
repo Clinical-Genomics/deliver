046e01351728838da5e44599113a24ee00efb610
kenny billiau <kenny.billiau@scilifelab.se>
2016-03-20 21:48:42 +0100

    Add AZ and AL to delivery email

--- scripts/checkfornewdemux.bash
+++ scripts/checkfornewdemux.bash
@@ -3,9 +3,8 @@
 source /mnt/hds/proj/bioinfo/SCRIPTS/log.bash
 log $(getversion)
 
-MAILTO=kenny.billiau@scilifelab.se,emma.sernstad@scilifelab.se,daniel.backman@scilifelab.se,robin.andeer@scilifelab.se
+MAILTO=kenny.billiau@scilifelab.se,emma.sernstad@scilifelab.se,daniel.backman@scilifelab.se,robin.andeer@scilifelab.se,anna.zetterlund@scilifelab.se,anna.leinfelt@scilifelab.se
 UNABASE=/mnt/hds/proj/bioinfo/DEMUX/
-ALIBASE=/mnt/hds/proj/bioinfo/ALIGN/
 runs=$(ls ${UNABASE})
 for run in ${runs[@]}; do
   if [ -f ${UNABASE}${run}/copycomplete.txt ]; then
@@ -19,11 +18,11 @@ for run in ${runs[@]}; do
       else
         log ${run} 'start trimming ...'
         NOW=$(date +"%Y%m%d%H%M%S")
-      	/home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/trimqxt.py ${UNABASE}${run} &> ${UNABASE}${run}/trimQXT.${NOW}.log
+        /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/trimqxt.py ${UNABASE}${run} &> ${UNABASE}${run}/trimQXT.${NOW}.log
       fi
 
       if [ ! -f ${UNABASE}${run}/trimming.txt ]; then
-        log ${run} 'copy is complete delivery is started' > ${UNABASE}${run}/delivery.txt 
+        log ${run} 'copy is complete delivery is started' > ${UNABASE}${run}/delivery.txt
         FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
 
         # add an X FC to clinstatsdb - because the permanent tunnel is not active on the nodes.
@@ -44,11 +43,10 @@ for run in ${runs[@]}; do
         NOW=$(date +"%Y%m%d%H%M%S")
         /home/hiseq.clinical/miniconda/envs/prod/bin/python /mnt/hds/proj/bioinfo/SCRIPTS/createfastqlinks.py ${FC} &> ${UNABASE}${run}/createfastqlinks.${FC}.${NOW}.log
 
-
-	SUBJECT=${FC}
-	# send an email on completion
-	log "cat ${UNABASE}${run}/stats*.txt | mail -s 'Run ${SUBJECT} COMPLETE!' ${MAILTO}"
-	cat ${UNABASE}${run}/stats*.txt | mail -s "Run ${SUBJECT} COMPLETE!" ${MAILTO}
+        SUBJECT=${FC}
+        # send an email on completion
+        log "column -t ${UNABASE}${run}/stats*.txt | mail -s 'Run ${SUBJECT} COMPLETE!' ${MAILTO}"
+        column -t ${UNABASE}${run}/stats*.txt | mail -s "Run ${SUBJECT} COMPLETE!" ${MAILTO}
       fi
     fi
   else
