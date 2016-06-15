abbbce7634c5d6888f123258113083b3e0511580
kenny billiau <kenny.billiau@scilifelab.se>
2016-06-15 20:12:54 +0200

    Feat rm previous files from MH FTP

--- scripts/mh-delivery.bash
+++ scripts/mh-delivery.bash
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+FILETYPE=TUMOR
+
 for FASTQ in "$@"; do
 
     FASTQ_DIR=$(dirname $FASTQ)
     FASTQ_FILE=$(basename $FASTQ)
 
     IFS=_ read -ra FASTQ_PARTS <<< "$FASTQ_FILE"
+    unset $IFS # reset IFS
 
     cd $FASTQ_DIR
 
     for READ_DIRECTION in 1 2; do
-        MH_FASTQ_FILENAME="${FASTQ_PARTS[0]}_${FASTQ_PARTS[2]}_${READ_DIRECTION}"
+        MH_FASTQ_FILENAME="${FASTQ_PARTS[0]}_${FILETYPE}_${FASTQ_PARTS[2]}_${READ_DIRECTION}"
         MH_FASTQ_FILE="${MH_FASTQ_FILENAME}.fastq.gz"
         LANE1_FASTQ_FILE="${FASTQ_PARTS[0]}_${FASTQ_PARTS[1]}_${FASTQ_PARTS[2]}_L001_R${READ_DIRECTION}_${FASTQ_PARTS[5]}"
         LANE2_FASTQ_FILE="${FASTQ_PARTS[0]}_${FASTQ_PARTS[1]}_${FASTQ_PARTS[2]}_L002_R${READ_DIRECTION}_${FASTQ_PARTS[5]}"
@@ -21,8 +24,8 @@ for FASTQ in "$@"; do
         echo "md5sum $MH_FASTQ_FILE > ${FASTQ_DIR}/${MH_FASTQ_FILE}.md5"
         md5sum $MH_FASTQ_FILE > ${FASTQ_DIR}/${MH_FASTQ_FILE}.md5
 
-        echo "lftp sftp://SFL:@ftp.de.molecularhealth.com/upload/ -e 'put ${MH_FASTQ_FILE}; put ${MH_FASTQ_FILE}.md5; bye;'"
-        lftp sftp://SFL:@ftp.de.molecularhealth.com/upload/ -e "put ${MH_FASTQ_FILE}; put ${MH_FASTQ_FILE}.md5; bye;"
+        echo "lftp sftp://SFL:@ftp.de.molecularhealth.com/upload/ -e 'rm -r -f *; put ${MH_FASTQ_FILE}; put ${MH_FASTQ_FILE}.md5; bye;'"
+        lftp sftp://SFL:@ftp.de.molecularhealth.com/upload/ -e "rm -r -f *; put ${MH_FASTQ_FILE}; put ${MH_FASTQ_FILE}.md5; bye;"
 
         echo "rm ${MH_FASTQ_FILE} ${MH_FASTQ_FILE}.md5"
         rm ${MH_FASTQ_FILE} ${MH_FASTQ_FILE}.md5
