e1ad7bce64f5d573a7f4f99bc437e689a8e479bf
kenny billiau <kenny.billiau@scilifelab.se>
2016-06-16 22:45:45 +0200

    Rename checkcustsamples.bash to linkcustsamples.bash

--- scripts/linkcustsamples.bash
+++ scripts/linkcustsamples.bash
@@ -3,7 +3,7 @@
 # check for md5sum file
 # for each line in md5sum file
 
-TICKET_FILE=${1?'Please provide a md5sum file: ticketID.md5sum'}
+TICKET_FILE=${1?'Please provide a md5sum file: ${ticketID}.md5sum'}
 INDIR=$(readlink -f $(dirname "${TICKET_FILE}"))
 
 cd /mnt/hds/proj/bioinfo/git/kenny/data-delivery/
@@ -17,4 +17,5 @@ while read -a LINE; do
 
     python -m createlinks.cli cust ${INDIR}/${FASTQ_FILE}
 done < ${TICKET_FILE}
+mv $TICKET_FILE ${TICKET_FILE}_complete
 cd -
