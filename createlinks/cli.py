0f9e6a0af6040feee7bcd95c43a03b9dad8ac38f
kenny billiau <kenny.billiau@scilifelab.se>
2016-06-24 22:49:32 +0200

    Bump version: 1.15.1 → 1.15.2

--- createlinks/cli.py
+++ createlinks/cli.py
@@ -9,7 +9,7 @@ from .modules.cust  import cust_links
 
 logger = logging.getLogger(__name__)
 
-__version__ = '1.15.1'
+__version__ = '1.15.2'
 
 @click.group()
 def link():
--- scripts/createbamlinks.py
+++ scripts/createbamlinks.py
@@ -11,7 +11,7 @@ from access import db
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '1.15.1'
+__version__ = '1.15.2'
 
 logger = logging.getLogger(__name__)
 
