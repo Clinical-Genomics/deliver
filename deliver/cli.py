3fb1b94a76c31b7727e1410052686ff3598ed435
kenny billiau <kenny.billiau@scilifelab.se>
2016-07-26 15:20:58 +0200

    Bump version: 1.17.0 → 1.17.1

--- deliver/cli.py
+++ deliver/cli.py
@@ -9,7 +9,7 @@ from .modules.cust  import cust_links
 
 logger = logging.getLogger(__name__)
 
-__version__ = '1.17.0'
+__version__ = '1.17.1'
 
 @click.group()
 def link():
--- scripts/link_external2mip.py
+++ scripts/link_external2mip.py
@@ -12,7 +12,7 @@ from glob import glob
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '1.17.0'
+__version__ = '1.17.1'
 
 logger = logging.getLogger(__name__)
 
