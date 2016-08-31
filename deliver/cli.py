a29c5e16cbbaecc6b0b031e20f64376aa321d491
kenny billiau <kenny.billiau@scilifelab.se>
2016-08-31 14:38:15 +0200

    Bump version: 1.18.0 → 1.18.1

--- deliver/cli.py
+++ deliver/cli.py
@@ -9,7 +9,7 @@ from .modules.cust  import cust_links
 
 logger = logging.getLogger(__name__)
 
-__version__ = '1.18.0'
+__version__ = '1.18.1'
 
 @click.group()
 def link():
--- deliver/modules/cust.py
+++ deliver/modules/cust.py
@@ -11,7 +11,7 @@ from access import db
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '1.18.0'
+__version__ = '1.18.1'
 
 logger = logging.getLogger(__name__)
 
