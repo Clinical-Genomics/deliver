4a9edb7c05aed9225f976d9c71ff5d962839ed91
Kenny Billiau <kenny.billiau@scilifelab.se>
2016-09-05 11:23:04 +0200

    Feat phase out auto QXT trimming
    Close #18

--- scripts/checkfornewdemux.bash
+++ scripts/checkfornewdemux.bash
@@ -11,43 +11,31 @@ for run in ${runs[@]}; do
     if [ -f ${UNABASE}${run}/delivery.txt ]; then
       log ${run} 'copy is complete and delivery has already started'
     else
-      if [ -f ${UNABASE}${run}/trimmed.txt ]; then
-        log ${run} 'trimming already finished'
-      elif [ -f ${UNABASE}${run}/trimming.txt ]; then
-        log ${run} 'trimming is in progress'
-      else
-        log ${run} 'start trimming ...'
-        NOW=$(date +"%Y%m%d%H%M%S")
-        python /mnt/hds/proj/bioinfo/SCRIPTS/trimqxt.py ${UNABASE}${run} &> ${UNABASE}${run}/trimQXT.${NOW}.log
+      log ${run} 'copy is complete delivery is started' > ${UNABASE}${run}/delivery.txt
+      FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
+
+      # add an X FC to clinstatsdb - because the permanent tunnel is not active on the nodes.
+      if [[ -d "${UNABASE}${run}/l1t11" ]]; then
+        log ${run} "python /mnt/hds/proj/bioinfo/SCRIPTS/xparseunaligned.py ${UNABASE}${run} &> ${UNABASE}${run}/LOG/xparseunaligned.`date +'%Y%m%d%H%M%S'`.log"
+        python /mnt/hds/proj/bioinfo/SCRIPTS/xparseunaligned.py ${UNABASE}${run} &> ${UNABASE}${run}/LOG/xparseunaligned.`date +'%Y%m%d%H%M%S'`.log
+
+        # create stats per project
+        for PROJECT in ${UNABASE}${run}/Unaligned/Project*; do
+          PROJECT=$(basename $PROJECT)
+          PROJECT_NR=${PROJECT##*_}
+          log ${run} "python /mnt/hds/proj/bioinfo/SCRIPTS/selectdemux.py $PROJECT_NR $FC &> ${UNABASE}${run}/stats-${PROJECT_NR}-${FC}.txt"
+          python /mnt/hds/proj/bioinfo/SCRIPTS/selectdemux.py $PROJECT_NR $FC &> ${UNABASE}${run}/stats-${PROJECT_NR}-${FC}.txt
+        done
       fi
+      # end add
 
-      if [ ! -f ${UNABASE}${run}/trimming.txt ]; then
-        log ${run} 'copy is complete delivery is started' > ${UNABASE}${run}/delivery.txt
-        FC=$(echo ${run} | awk 'BEGIN {FS="/"} {split($(NF-1),arr,"_");print substr(arr[4],2,length(arr[4]))}')
+      NOW=$(date +"%Y%m%d%H%M%S")
+      python /mnt/hds/proj/bioinfo/SCRIPTS/createfastqlinks.py ${FC} &> ${UNABASE}${run}/createfastqlinks.${FC}.${NOW}.log
 
-        # add an X FC to clinstatsdb - because the permanent tunnel is not active on the nodes.
-        if [[ -d "${UNABASE}${run}/l1t11" ]]; then
-          log ${run} "python /mnt/hds/proj/bioinfo/SCRIPTS/xparseunaligned.py ${UNABASE}${run} &> ${UNABASE}${run}/LOG/xparseunaligned.`date +'%Y%m%d%H%M%S'`.log"
-          python /mnt/hds/proj/bioinfo/SCRIPTS/xparseunaligned.py ${UNABASE}${run} &> ${UNABASE}${run}/LOG/xparseunaligned.`date +'%Y%m%d%H%M%S'`.log
-
-          # create stats per project
-          for PROJECT in ${UNABASE}${run}/Unaligned/Project*; do
-            PROJECT=$(basename $PROJECT)
-            PROJECT_NR=${PROJECT##*_}
-            log ${run} "python /mnt/hds/proj/bioinfo/SCRIPTS/selectdemux.py $PROJECT_NR $FC &> ${UNABASE}${run}/stats-${PROJECT_NR}-${FC}.txt"
-            python /mnt/hds/proj/bioinfo/SCRIPTS/selectdemux.py $PROJECT_NR $FC &> ${UNABASE}${run}/stats-${PROJECT_NR}-${FC}.txt
-          done
-        fi
-        # end add
-
-        NOW=$(date +"%Y%m%d%H%M%S")
-        python /mnt/hds/proj/bioinfo/SCRIPTS/createfastqlinks.py ${FC} &> ${UNABASE}${run}/createfastqlinks.${FC}.${NOW}.log
-
-        SUBJECT=${FC}
-        # send an email on completion
-        log "column -t ${UNABASE}${run}/stats*.txt | mail -s 'Run ${SUBJECT} COMPLETE!' ${MAILTO}"
-        column -t ${UNABASE}${run}/stats*.txt | mail -s "Run ${SUBJECT} COMPLETE!" ${MAILTO}
-      fi
+      SUBJECT=${FC}
+      # send an email on completion
+      log "column -t ${UNABASE}${run}/stats*.txt | mail -s 'Run ${SUBJECT} COMPLETE!' ${MAILTO}"
+      column -t ${UNABASE}${run}/stats*.txt | mail -s "Run ${SUBJECT} COMPLETE!" ${MAILTO}
     fi
   else
     log ${run} 'is not yet completely copied'
