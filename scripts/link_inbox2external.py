28c1747559667e9ffe0bbfcfc333c5a6836a91de
kenny billiau <kenny.billiau@scilifelab.se>
2016-09-07 14:12:14 +0200

    Bump version: 1.19.0 → 1.19.1

--- scripts/link_inbox2external.py
+++ scripts/link_inbox2external.py
@@ -8,7 +8,7 @@ from access import db
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '1.19.0'
+__version__ = '1.19.1'
 
 logger = logging.getLogger(__name__)
 
