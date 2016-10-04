367b4b338382233cce7f0f27bc06748d34d5f6ff
kenny billiau <kenny.billiau@scilifelab.se>
2016-10-04 12:52:32 +0200

    Bump version: 1.20.0 → 1.20.1

--- scripts/createbamlinks.py
+++ scripts/createbamlinks.py
@@ -11,7 +11,7 @@ from access import db
 from genologics.lims import *
 from genologics.config import BASEURI, USERNAME, PASSWORD
 
-__version__ = '1.20.0'
+__version__ = '1.20.1'
 
 logger = logging.getLogger(__name__)
 
