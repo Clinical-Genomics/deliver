f25aa5be0887741c3a0401ee8b555e3a418bf3d1
Kenny Billiau <kenny.billiau@scilifelab.se>
2017-01-17 14:34:18 +0100

    Fix dest dir for ext samples

--- deliver/modules/ext.py
+++ deliver/modules/ext.py
@@ -175,7 +175,7 @@ def ext_links(start_dir, outdir):
             index = get_index(fastq_full_file_name)
 
         # create dest dir
-        complete_outdir = os.path.join(outdir, cust_name, family_id, seq_type_dir, sample_id, 'fastq')
+        complete_outdir = os.path.join(outdir, 'customers', cust_name, family_id, seq_type_dir, sample_id, 'fastq')
         out_filename = '_'.join( [lane, date, FC, sample_id, index, direction ])
         out_full_filename = os.path.join(complete_outdir, out_filename)
         logger.debug(complete_outdir)
