15c2d314989fc2dac504b702175e5da685c69d22
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-02-03 10:26:57 +0100

    Bump version: 1.24.2 → 1.25.0

--- deliver/cli.py
+++ deliver/cli.py
@@ -11,7 +11,7 @@ from .ext import ext
 
 log = logging.getLogger(__name__)
 
-__version__ = '1.24.2'
+__version__ = '1.25.0'
 
 
 @click.group()
--- scripts/trimqxt.py
+++ scripts/trimqxt.py
@@ -10,7 +10,7 @@ import datetime
 import shutil
 from access import db, lims
 
-__version__ = '1.24.2'
+__version__ = '1.25.0'
 
 logger = logging.getLogger(__name__)
 
