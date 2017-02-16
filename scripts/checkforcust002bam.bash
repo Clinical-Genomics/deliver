e4566436dd093053739f0a82f22f870e64528ce6
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-02-16 22:35:39 +0100

    Add auto bamvcf linking for cust002

--- scripts/checkforcust002bam.bash
+++ scripts/checkforcust002bam.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-set -e
+shopt -s expand_aliases
+source ~/.bashrc
+
+set -eu
 
 log() {
     NOW=$(date +"%Y%m%d%H%M%S")
     echo "[${NOW}] $@"
 }
 
-cd /mnt/hds/proj/bioinfo/git/kenny/data-delivery/
-find /mnt/hds/proj/bioinfo/MIP_ANALYSIS/cust002/*/genomes/ -name '*_qc_sampleInfo.yaml' -mtime +1 -mtime -2 -exec python -m createlinks.cli bam $SAMPLE_INFO \;
-cd -
+
+cases=$(scout cases --institute cust002 --finished)
+for case in ${cases[@]}; do
+    deliver_bamvcfs ${case}
+done
