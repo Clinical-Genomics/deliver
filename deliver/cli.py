892cf80778bed0b3316652b78598e979ce965f1a
Kenny Billiau <Kenny.billiau@scilifelab.se>
2017-03-30 14:43:40 +0200

    Bump version: 1.30.0 → 1.30.1

--- deliver/cli.py
+++ deliver/cli.py
@@ -12,7 +12,7 @@ from .ext import ext
 
 log = logging.getLogger(__name__)
 
-__version__ = '1.30.0'
+__version__ = '1.30.1'
 DEMUXDIR='/mnt/hds/proj/bioinfo/DEMUX/'
 
 
--- scripts/trimqxt.py
+++ scripts/trimqxt.py
@@ -10,7 +10,7 @@ import datetime
 import shutil
 from access import db, lims
 
-__version__ = '1.30.0'
+__version__ = '1.30.1'
 
 logger = logging.getLogger(__name__)
 
