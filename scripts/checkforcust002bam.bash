951b62fd4f816c0a6e6706abefeee5af6af5e054
Kenny Billiau <kenny.billiau@scilifelab.se>
2017-04-12 10:35:16 +0200

    Fix checkforcust002bamfiles.bash

--- scripts/checkforcust002bam.bash
+++ scripts/checkforcust002bam.bash
@@ -3,17 +3,16 @@
 shopt -s expand_aliases
 source ~/.bashrc
 
-set -eu
-
 log() {
     NOW=$(date +"%Y%m%d%H%M%S")
     echo "[${NOW}] $@"
 }
 
 
-cases=$(housekeeper runs --output case --limit 1000 --after `date +'%Y-%m-%d' -d '15 days ago'`)
-for case in ${cases[@]}; do
-    case=${case%% - *}
+cases=($(housekeeper runs --output case --limit 1000 --after `date +'%Y-%m-%d' -d '15 days ago'`))
+for case in "${cases[@]}"; do
+    if ! [[ $case =~ cust* ]]; then continue; fi
+    log "working on $case"
     cust=${case%%-*}
     if [[ $cust != 'cust002' ]]; then continue; fi
     deliver_bamvcfs ${case}
