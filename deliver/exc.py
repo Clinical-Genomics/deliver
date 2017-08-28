a6248a6552f9ba5bfb963b418b007e60cecca638
Robin Andeer <robin.andeer@gmail.com>
2017-08-28 14:56:24 +0200

    add function to name fastq files update getting fastq files from demux

--- deliver/exc.py
+++ deliver/exc.py
@@ -2,5 +2,15 @@
 from sqlalchemy.orm.exc import NoResultFound
 
 
-class MissingFlowcellError(NoResultFound):
+class DeliverError(Exception):
+
+    def __init__(self, message: str=None):
+        self.message = message
+
+
+class MissingFlowcellError(NoResultFound, DeliverError):
+    pass
+
+
+class FastqFileMissingError(DeliverError):
     pass
--- deliver/modules/analysis.py
+++ deliver/modules/analysis.py
@@ -1,19 +1,38 @@
 import logging
+import datetime as dt
 from pathlib import Path
 from typing import List
 
 import cglims
 import cgstats
 
+from deliver.exc import DeliverError, FastqFileMissingError
+
 LOG = logging.getLogger(__name__)
 
 
-def get_fastq(root_dir: str, flowcell: str, sample: str, lane: int, tile: str=None, read: int=None):
-    pattern = f"{root_dir}/*{flowcell}/Unaligned*/Project_*/Sample_{sample}"
-    filename = "{lane}_{tile}*_{read}.fastq.gz"
-    if tile:
-        filename = "".format(tile=tile or 'NOTHING')
-        filename.replace('_NOTHING_', '')
+def get_fastq(root_dir: str, flowcell: str, sample: str, lane: int, pooled: bool=False):
+    """Find FASTQ file."""
+    directory = Path(f"{root_dir}/*{flowcell}/Unaligned*/Project_*/Sample_{sample}")
+    filename = f"{lane}*_{{1,2}}.fastq.gz"
+    files = [file_ for file_ in directory.glob(filename) if
+             (not pooled or ('Undetermined' not in file_))]
+    files_found = len(files)
+    if files_found == (2 if pooled else 4):
+        return files
+    elif files_found < (2 if pooled else 4):
+        raise FastqFileMissingError(directory / filename)
+    elif files_found > (2 if pooled else 4):
+        raise DeliverError(f"too many files found: {directory / filename}")
+
+
+def get_mipname(lane: int, flowcell: str, sample: str, read: int, undetermined: bool=False,
+                date: dt.datetime=None, index: str=None) -> str:
+    """Name a FASTQ file following MIP conventions."""
+    flowcell = f"{flowcell}-undetermined" if undetermined else flowcell
+    date_str = (date or dt.datetime.now()).strftime("%y%m%d")
+    index = index if index else 'XXXXXX'
+    return f"{lane}_{date_str}_{flowcell}_{sample}_{index}_{read}.fastq.gz"
 
 
 def link_mip(root_dir: str, fastq_path: str, dest_name: str, customer: str, family: str,
