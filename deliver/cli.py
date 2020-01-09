46dd270aa25fe63059eae94c03c18e773dfde01e
Kenny Billiau <kenny.billiau@scilifelab.se>
2020-01-09 13:25:04 +0100

    Bump version: 2.3.1 → 2.4.0

--- deliver/cli.py
+++ deliver/cli.py
@@ -16,7 +16,7 @@ from .modules.db import CgStats
 
 LOG = logging.getLogger(__name__)
 
-__version__ = '2.3.1'
+__version__ = '2.4.0'
 
 @click.group()
 @click.option('-l', '--log-level', default='INFO', envvar='LOGLEVEL')
--- scripts/trimqxt.py
+++ scripts/trimqxt.py
@@ -10,7 +10,7 @@ import datetime
 import shutil
 from access import db, lims
 
-__version__ = '2.3.1'
+__version__ = '2.4.0'
 
 logger = logging.getLogger(__name__)
 
